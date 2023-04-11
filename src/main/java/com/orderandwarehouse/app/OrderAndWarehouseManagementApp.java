3ce10f7e8afd8ab97bb050758167a3ffaf840674
T.Sanyi <s.truczkai@gmail.com>
2023-04-11 18:14:25 +0200

    add starter project

---
+++ src/main/java/com/orderandwarehouse/app/OrderAndWarehouseManagementApp.java
@@ -0,0 +1,13 @@
+package com.orderandwarehouse.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderAndWarehouseManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAndWarehouseManagementApp.class, args);
+	}
+
+}
