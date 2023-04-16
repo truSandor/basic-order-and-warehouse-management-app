93f0e1742d55b6459511b4ace3fbce345760ef39
T.Sanyi <s.truczkai@gmail.com>
2023-04-16 21:12:56 +0200

    fix component data

--- src/main/resources/db/migration/V2__add_components.sql
+++ src/main/resources/db/migration/V2__add_components.sql
@@ -1,23 +1,23 @@
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'capacitor 1206 4nF 10V', 1, 4, 'nF', 10, 'V', 0.1, true );
+values ( 'capacitor 1206 4nF 10V', 0, 4, 'nF', 10, 'V', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'capacitor 1206 56nF 10V', 1, 56, 'nF', 10, 'V', 0.1, true );
+values ( 'capacitor 1206 56nF 10V', 0, 56, 'nF', 10, 'V', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'capacitor 1206 56pF 10V', 1, 56, 'pF', 10, 'V', 0.1, true );
+values ( 'capacitor 1206 56pF 10V', 0, 56, 'pF', 10, 'V', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'capacitor 0603 4nF 100V', 1, 4, 'nF', 100, 'V', 0.1, true );
+values ( 'capacitor 0603 4nF 100V', 0, 4, 'nF', 100, 'V', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'resistor 1206 1R 1W 5%', 1, 1, 'Ohm', 1, 'W', 0.1, true );
+values ( 'resistor 1206 1R 1W 5%', 0, 1, 'Ohm', 1, 'W', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'resistor 0402 100R 3W 5%', 1, 100, 'Ohm', 3, 'W', 0.1, true );
+values ( 'resistor 0402 100R 3W 5%', 0, 100, 'Ohm', 3, 'W', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'resistor 0603 220K 1W 1%', 1, 220, 'KOhm', 1, 'W', 0.1, true );
+values ( 'resistor 0603 220K 1W 1%', 0, 220, 'KOhm', 1, 'W', 0.1, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'transformer 220/24V AC 20W', 2, 220, 'V', 24, 'V', 100, true );
+values ( 'transformer 220/24V AC 20W', 1, 220, 'V', 24, 'V', 100, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'relay RIF-0-RPT-24DC/ 1', 2, 24, 'V', 9, 'mA', 25, true );
+values ( 'relay RIF-0-RPT-24DC/ 1', 1, 24, 'V', 9, 'mA', 25, true );
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, "visible")
-values ( 'Electrical tape black 30mx2cm', 3, 30, 'm', 2, 'cm', 30, true );
+values ( 'Electrical tape black 30mx2cm', 2, 30, 'm', 2, 'cm', 30, true );
 
 insert into component ("name", "type", primary_value, primary_unit, secondary_value, secondary_unit, weight_in_grammes, tolerance, "visible")
-values ( 'resistor 470k 3W 5%', 2, 470, 'KOhm', 3, 'W', 0.1, 5, true );
+values ( 'resistor 470k 3W 5%', 1, 470, 'KOhm', 3, 'W', 0.1, 5, true );
