d25e58829865b6687f7ec075eda0f68975d9ae64
T.Sanyi <s.truczkai@gmail.com>
2023-04-16 15:31:00 +0200

    add method to find all rows of a partslist

--- src/main/java/com/orderandwarehouse/app/repository/PartsListRowDao.java
+++ src/main/java/com/orderandwarehouse/app/repository/PartsListRowDao.java
@@ -4,6 +4,9 @@ import com.orderandwarehouse.app.model.PartsListRow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartsListRowDao extends JpaRepository<PartsListRow, Long> {
+    List<PartsListRow> findAllByVisibleTrueAndAndPartsListIdOrderById(Long partsListId);
 }
