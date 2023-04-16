e6ce4ed64bb44d768923f57fb46837e18131374a
T.Sanyi <s.truczkai@gmail.com>
2023-04-16 14:08:22 +0200

    add endpoint to get all visible Components

--- src/main/java/com/orderandwarehouse/app/controller/ComponentController.java
+++ src/main/java/com/orderandwarehouse/app/controller/ComponentController.java
@@ -1,11 +1,16 @@
 package com.orderandwarehouse.app.controller;
 
+import com.orderandwarehouse.app.model.Component;
 import com.orderandwarehouse.app.service.ComponentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/components")
@@ -15,7 +20,7 @@ public class ComponentController {
     private final ComponentService service;
 
     @GetMapping
-    public String testGetMapping(){
-        return "Components will be listed here!";
+    public ResponseEntity<List<Component>> getAll() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 }
--- src/main/java/com/orderandwarehouse/app/service/ComponentService.java
+++ src/main/java/com/orderandwarehouse/app/service/ComponentService.java
@@ -1,13 +1,21 @@
 package com.orderandwarehouse.app.service;
 
+import com.orderandwarehouse.app.model.Component;
 import com.orderandwarehouse.app.repository.ComponentDao;
 import com.orderandwarehouse.app.repository.StorageUnitDao;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ComponentService {
     private final ComponentDao componentDao;
     private final StorageUnitDao storageUnitDao;
+
+    public List<Component> getAll() {
+        return componentDao.findAllByVisibleTrueOrderByIdAsc();
+    }
 }
