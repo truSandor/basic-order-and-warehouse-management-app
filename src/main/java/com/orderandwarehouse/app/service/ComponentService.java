55a4c4d5fe62505bb1bcb69ce95a88b272f3c39f
T.Sanyi <s.truczkai@gmail.com>
2023-04-20 20:44:00 +0200

    implement methods in ComponentService

--- src/main/java/com/orderandwarehouse/app/service/ComponentService.java
+++ src/main/java/com/orderandwarehouse/app/service/ComponentService.java
@@ -1,21 +1,105 @@
 package com.orderandwarehouse.app.service;
 
-import com.orderandwarehouse.app.model.Component;
+import com.orderandwarehouse.app.exception.ComponentStillInUseException;
+import com.orderandwarehouse.app.model.*;
 import com.orderandwarehouse.app.repository.ComponentDao;
+import com.orderandwarehouse.app.repository.PartsListRowDao;
+import com.orderandwarehouse.app.repository.ProductDao;
 import com.orderandwarehouse.app.repository.StorageUnitDao;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ComponentService {
     private final ComponentDao componentDao;
     private final StorageUnitDao storageUnitDao;
+    private final PartsListRowDao partsListRowDao;
+    private final ProductDao productDao;
 
     public List<Component> getAll() {
         return componentDao.findAllByVisibleTrueOrderByIdAsc();
+    }
+
+    public Optional<Component> getById(Long id) {
+        return componentDao.findByIdAndVisibleTrue(id);
+    }
+
+    public Component add(@Valid Component component) {
+        return componentDao.save(component);
+    }
+
+    public Optional<Component> update(Long id, @Valid Component component) {
+        var maybeComponent = componentDao.findByIdAndVisibleTrue(id);
+        component.setId(id);
+        return maybeComponent.map(mc -> {
+            component.setId(id);
+            return componentDao.save(component);
+        }); //not sure if this return statement works correctly
+    }
+
+    public boolean softDelete(Long id) throws SQLException {
+        List<StorageUnit> storageUnits = storageUnitDao.findStorageUnitsByComponent_Id(id);
+        List<PartsListRow> partsListRows = partsListRowDao.findAllByVisibleTrueAndComponent_Id(id);
+        if (storageUnits.isEmpty() && partsListRows.isEmpty()) {
+            Integer linesModifiedCount = componentDao.setInvisibleById(id);
+            switch (linesModifiedCount) {
+                case 0 -> throw new NoSuchElementException(String.format("Component '%d' not found!", id));
+                case 1 -> {
+                    return true;
+                }
+                //should never happen, because IDs are unique
+                default ->
+                        throw new SQLException(String.format("Error multiple component with the same ID '%d' updated!", id));
+            }
+        } else {
+            throw new ComponentStillInUseException(createExceptionMessage(id, partsListRows, storageUnits));
+        }
+    }
+
+
+    private String createExceptionMessage(Long id, List<PartsListRow> partsListRows, List<StorageUnit> storageUnits) {
+        return String.format(
+                "Component '%d' found in parts list(s) of product(s): %s%sRemove component from parts list(s) first!%s" +
+                        "and/or%s" +
+                        "Component '%d' found in storage units: %s%sEmpty the storage units first!",
+                id,
+                getProductIds(partsListRows),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                System.lineSeparator(),
+                id,
+                getStorageUnitIds(storageUnits),
+                System.lineSeparator()
+        );
+    }
+
+    private List<Long> getStorageUnitIds(List<StorageUnit> storageUnits) {
+        return storageUnits.stream().map(StorageUnit::getId).toList();
+    }
+
+    private List<Long> getProductIds(List<PartsListRow> partsListRows) {
+        return productDao.findAllByVisibleTrueAndPartsListIdIsIn(getPartListIds(partsListRows)).stream()
+                .map(Product::getId)
+                .toList();
+    }
+
+    private List<Long> getPartListIds(List<PartsListRow> partsListRows) {
+        return partsListRows.stream()
+                .map(PartsListRow::getPartsList)
+                .filter(PartsList::isVisible)
+                .map(PartsList::getId)
+                .toList();
+    }
+
+    public List<Component> getByNameLike(String nameLike) {
+        return componentDao.findAllByNameIsContainingIgnoreCaseAndVisibleTrue(nameLike);
     }
 }
