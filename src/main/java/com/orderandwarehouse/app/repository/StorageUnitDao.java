78283c31aa0c2093ea9ecaba3912190235139473
T.Sanyi <s.truczkai@gmail.com>
2023-04-23 21:39:39 +0200

    add new methods to StorageUnitDao

--- src/main/java/com/orderandwarehouse/app/repository/StorageUnitDao.java
+++ src/main/java/com/orderandwarehouse/app/repository/StorageUnitDao.java
@@ -2,13 +2,24 @@ package com.orderandwarehouse.app.repository;
 
 import com.orderandwarehouse.app.model.StorageUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StorageUnitDao extends JpaRepository<StorageUnit, Long> {
     List<StorageUnit> findAllByVisibleTrueOrderByIdAsc();
 
-    List<StorageUnit> findStorageUnitsByComponent_Id(Long componentId);
+
+    Optional<StorageUnit> findByIdAndVisibleTrue(Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update StorageUnit su set su.visible = false where su.id = :id")
+    Integer setInvisibleById(@Param("id") Long id);
+
+    List<StorageUnit> findAllByVisibleTrueAndComponent_Id(Long ComponentId);
 }
