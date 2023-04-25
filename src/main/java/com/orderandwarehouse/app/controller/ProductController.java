e6a3f77be61d4f08a6f936ed98f74a197248984f
T.Sanyi <s.truczkai@gmail.com>
2023-04-25 17:44:28 +0200

    add getById() and add() endpoints to ProductController

--- src/main/java/com/orderandwarehouse/app/controller/ProductController.java
+++ src/main/java/com/orderandwarehouse/app/controller/ProductController.java
@@ -1,14 +1,13 @@
 package com.orderandwarehouse.app.controller;
 
+import com.orderandwarehouse.app.converter.ProductConverter;
 import com.orderandwarehouse.app.model.Product;
+import com.orderandwarehouse.app.model.dto.ProductDto;
 import com.orderandwarehouse.app.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +17,22 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class ProductController {
     private final ProductService service;
+    private final ProductConverter converter;
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAll(){
+    public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getById(@PathVariable Long id) {
+        return service.getById(id)
+                .map(p -> new ResponseEntity<>(p, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> add(@RequestBody ProductDto productDto) {
+        return new ResponseEntity<>(service.add(converter.dtoToEntity(productDto)), HttpStatus.OK);
     }
 }
