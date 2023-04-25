1ba3bf31f648b0a4c50b3b2f8f7d0d506c5dd4f4
T.Sanyi <s.truczkai@gmail.com>
2023-04-25 17:44:05 +0200

    add getById() and add() methods to ProductService

--- src/main/java/com/orderandwarehouse/app/service/ProductService.java
+++ src/main/java/com/orderandwarehouse/app/service/ProductService.java
@@ -4,10 +4,12 @@ import com.orderandwarehouse.app.model.Product;
 import com.orderandwarehouse.app.repository.PartsListDao;
 import com.orderandwarehouse.app.repository.PartsListRowDao;
 import com.orderandwarehouse.app.repository.ProductDao;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,13 @@ public class ProductService {
 
     public List<Product> getAll() {
         return productDao.findAllByVisibleTrueOrderByNameAscVersionAscIdAsc();
+    }
+
+    public Optional<Product> getById(Long id) {
+        return productDao.findByIdAndVisibleTrue(id);
+    }
+
+    public Product add(@Valid Product product) {
+        return productDao.save(product);
     }
 }
