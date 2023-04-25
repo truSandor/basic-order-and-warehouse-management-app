f8b351fc0a366a2e7eb78aa6842db0a7aea6a5e4
T.Sanyi <s.truczkai@gmail.com>
2023-04-25 17:42:18 +0200

    add findByIdAndVisibleTrue() to ProductDao

--- src/main/java/com/orderandwarehouse/app/repository/ProductDao.java
+++ src/main/java/com/orderandwarehouse/app/repository/ProductDao.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Long> {
     List<Product> findAllByVisibleTrueOrderByNameAscVersionAscIdAsc();
 
     List<Product> findAllByVisibleTrueAndPartsListIdIsIn(List<Long> partListIds);
+
+    Optional<Product> findByIdAndVisibleTrue(Long id);
 }
