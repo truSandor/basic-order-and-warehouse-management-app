9ea0e309f4fe3cff83d20abdcc4ed42309d09a59
T.Sanyi <s.truczkai@gmail.com>
2023-04-26 17:55:08 +0200

    start implementing softDelete in ProductService

--- src/main/java/com/orderandwarehouse/app/repository/ProductDao.java
+++ src/main/java/com/orderandwarehouse/app/repository/ProductDao.java
@@ -2,6 +2,9 @@ package com.orderandwarehouse.app.repository;
 
 import com.orderandwarehouse.app.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,8 @@ public interface ProductDao extends JpaRepository<Product, Long> {
     List<Product> findAllByVisibleTrueOrderByNameAscVersionAscIdAsc();
 
     Optional<Product> findByIdAndVisibleTrue(Long id);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Product p set p.visible = false where p.id = :id")
+    Integer setInvisibleById(@Param("id") Long id);
 }
--- src/main/java/com/orderandwarehouse/app/service/ProductService.java
+++ src/main/java/com/orderandwarehouse/app/service/ProductService.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -36,4 +37,27 @@ public class ProductService {
         product.setWeightInGrammes(dto.getWeightInGrammes());
         return productDao.save(product);
     }
+
+//    public boolean softDelete(Long id) throws SQLException {
+//        Product product = productDao.findByIdAndVisibleTrue(id)
+//                .orElseThrow(() -> new NoSuchElementException(String.format("Product '%d' not found!", id)));
+//        if (product.getPartsList() != null && !product.getPartsList().isEmpty() )
+//            throw new ProductHasPartsListException(String.format("Product '%d' has part!", id)));
+//        if ((product.getPartsList() == null || product.getPartsList().isEmpty()) &&
+//                (product.getOrders() == null || product.getOrders().isEmpty() || product.hasNoActiveOrders())) {
+//            Integer linesModifiedCount = productDao.setInvisibleById(id);
+//            if (linesModifiedCount == 1) {
+//                return true;
+//            } else {
+//                //should never happen, because IDs are unique
+//                throw new SQLException(String.format("Error multiple products with the same ID '%d' updated!", id));
+//            }
+//        } else {
+//            throw new ProductStillInUseException(
+//                    String.format(
+//                            "!",
+//                            id,
+//                            product.getId()));
+//        }
+//    }
 }
