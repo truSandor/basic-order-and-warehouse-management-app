427c1fa144e50129d38fce5c86b9c625051416b3
T.Sanyi <s.truczkai@gmail.com>
2023-04-27 15:24:16 +0200

    add orderDto

---
+++ src/main/java/com/orderandwarehouse/app/model/dto/OrderDto.java
@@ -0,0 +1,21 @@
+package com.orderandwarehouse.app.model.dto;
+
+import com.orderandwarehouse.app.model.Status;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDto {
+    private Long  productId;
+    private Integer quantity;
+    private LocalDateTime dateReceived;
+    private LocalDateTime deadline;
+    private LocalDateTime dateStarted;
+    private LocalDateTime dateCompleted;
+    private Status status;
+}
