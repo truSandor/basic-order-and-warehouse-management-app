a0c2e6074ff35e02bf04de98bbf35d784774e247
T.Sanyi <s.truczkai@gmail.com>
2023-04-27 23:03:39 +0200

    create custom query for deleteAllByProductId()

--- src/main/java/com/orderandwarehouse/app/repository/PartsListRowDao.java
+++ src/main/java/com/orderandwarehouse/app/repository/PartsListRowDao.java
@@ -2,6 +2,9 @@ package com.orderandwarehouse.app.repository;
 
 import com.orderandwarehouse.app.model.PartsListRow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,5 +15,7 @@ public interface PartsListRowDao extends JpaRepository<PartsListRow, Long> {
     List<PartsListRow> findAllByProductId(Long productId);
 
     @Transactional
-    void deleteByProduct_Id(Long product_id);
+    @Modifying(clearAutomatically = true)
+    @Query("delete from PartsListRow plr where plr.product.id = :product_id")
+    void deleteByProduct_Id(@Param("product_id") Long productId);
 }
