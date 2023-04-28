c830d480f47b7ea3e1b0111776054ab7e1ca09e5
T.Sanyi <s.truczkai@gmail.com>
2023-04-28 22:48:14 +0200

    rework OrderInProgressException

--- src/main/java/com/orderandwarehouse/app/exception/OrderInProgressException.java
+++ src/main/java/com/orderandwarehouse/app/exception/OrderInProgressException.java
@@ -1,13 +1,16 @@
 package com.orderandwarehouse.app.exception;
 
+import com.orderandwarehouse.app.model.Status;
 import lombok.Getter;
 
 @Getter
 public class OrderInProgressException extends IllegalStateException {
     private Long orderId;
+    private Status status;
 
-    public OrderInProgressException(Long orderId) {
-        super(String.format("Order '%d' is in progress!", orderId));
+    public OrderInProgressException(Long orderId, Status status) {
+        super(String.format("Order '%d' is %s, and can't be deleted!", orderId, status.toString()));
         this.orderId = orderId;
+        this.status = status;
     }
 }
