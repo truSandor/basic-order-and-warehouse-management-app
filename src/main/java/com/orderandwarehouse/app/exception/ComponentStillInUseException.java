0c94f947b38b5e2f0a943ef8c1ee0216ec6061c8
T.Sanyi <s.truczkai@gmail.com>
2023-04-28 20:41:42 +0200

    rework custom Exceptions

--- src/main/java/com/orderandwarehouse/app/exception/ComponentStillInUseException.java
+++ src/main/java/com/orderandwarehouse/app/exception/ComponentStillInUseException.java
@@ -1,7 +1,19 @@
 package com.orderandwarehouse.app.exception;
 
-public class ComponentStillInUseException extends IllegalStateException{
-    public ComponentStillInUseException(String s) {
-        super(s);
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ComponentStillInUseException extends IllegalStateException {
+    private Long componentId;
+    private List<Long> storageUnitIds;
+    private List<Long> productIds;
+
+    public ComponentStillInUseException(Long componentId, List<Long> storageUnitIds, List<Long> productIds) {
+        super(String.format("Component '%d' is still in use!", componentId));
+        this.componentId = componentId;
+        this.storageUnitIds = storageUnitIds;
+        this.productIds = productIds;
     }
 }
--- src/main/java/com/orderandwarehouse/app/exception/StorageUnitStillInUseException.java
+++ src/main/java/com/orderandwarehouse/app/exception/StorageUnitStillInUseException.java
@@ -1,7 +1,17 @@
 package com.orderandwarehouse.app.exception;
 
+import lombok.Getter;
+
+@Getter
 public class StorageUnitStillInUseException extends IllegalStateException {
-    public StorageUnitStillInUseException(String s) {
-        super(s);
+    private Long storageUnitId;
+    private Long componentId;
+    private Double quantity;
+
+    public StorageUnitStillInUseException(Long storageUnitId, Long componentId, Double quantity) {
+        super(String.format("Storage Unit '%d' is still in use", storageUnitId));
+        this.storageUnitId = storageUnitId;
+        this.componentId = componentId;
+        this.quantity = quantity;
     }
 }
