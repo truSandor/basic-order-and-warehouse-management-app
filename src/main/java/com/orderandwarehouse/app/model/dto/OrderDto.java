dd1112e6cf5ea876e3504d0b5efd62d0622eae19
T.Sanyi <s.truczkai@gmail.com>
2023-04-30 18:34:49 +0200

    add validation constraints to OrderDto

--- src/main/java/com/orderandwarehouse/app/model/dto/OrderDto.java
+++ src/main/java/com/orderandwarehouse/app/model/dto/OrderDto.java
@@ -1,18 +1,34 @@
 package com.orderandwarehouse.app.model.dto;
 
 import com.orderandwarehouse.app.model.Status;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class OrderDto {
-    private Long  productId;
+
+    @Min(value = 1)
+    private Long id;
+    @NotNull
+    @Min(value = 1)
+    private Long productId;
+    @NotNull
+    @Min(value = 1)
     private Integer quantity;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-ddThh:mm:ss.SS")
     private LocalDateTime dateReceived;
+    @DateTimeFormat(pattern = "yyyy-MM-ddThh:mm:ss.SS")
     private LocalDateTime deadline;
+    @DateTimeFormat(pattern = "yyyy-MM-ddThh:mm:ss.SS")
     private LocalDateTime dateStarted;
+    @DateTimeFormat(pattern = "yyyy-MM-ddThh:mm:ss.SS")
     private LocalDateTime dateCompleted;
+    @NotNull
     private Status status;
 }
