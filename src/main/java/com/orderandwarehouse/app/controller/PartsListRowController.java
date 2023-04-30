a2e6c2ccc510e2f72042323b618b90d693d3e6c2
T.Sanyi <s.truczkai@gmail.com>
2023-04-30 19:40:31 +0200

    add validation to PartsListRowController

--- src/main/java/com/orderandwarehouse/app/controller/PartsListRowController.java
+++ src/main/java/com/orderandwarehouse/app/controller/PartsListRowController.java
@@ -4,9 +4,12 @@ import com.orderandwarehouse.app.model.PartsListRow;
 import com.orderandwarehouse.app.model.dto.PartsListRowDto;
 import com.orderandwarehouse.app.service.PartsListRowService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.Set;
 @RestController
 @RequestMapping("/products/partslist")
 @RequiredArgsConstructor
+@Validated
 public class PartsListRowController {
     private final PartsListRowService service;
 
@@ -29,17 +33,17 @@ public class PartsListRowController {
     }
 
     @PostMapping("/{productId}")
-    public ResponseEntity<List<PartsListRow>> addAllToProduct(@PathVariable Long productId, @RequestBody @Valid List<PartsListRowDto> partsList) {
+    public ResponseEntity<List<PartsListRow>> addAllToProduct(@PathVariable @NotNull @Min(value = 1) Long productId, @RequestBody @Valid List<PartsListRowDto> partsList) {
         return new ResponseEntity<>(service.addAllToProduct(productId, partsList), HttpStatus.OK);
     }
 
     @PutMapping("/{productId}")
-    public ResponseEntity<List<PartsListRow>> updateAllBelongingToProduct(@PathVariable Long productId, @RequestBody @Valid Set<PartsListRowDto> partsList) {
+    public ResponseEntity<List<PartsListRow>> updateAllBelongingToProduct(@PathVariable @NotNull @Min(value = 1) Long productId, @RequestBody @Valid Set<PartsListRowDto> partsList) {
         return new ResponseEntity<>(service.updateAllBelongingToProduct(productId, partsList), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/single/row/{id}")
-    public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
+    public ResponseEntity<HttpStatus> delete(@PathVariable @NotNull @Min(value = 1) Long id) {
         service.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
