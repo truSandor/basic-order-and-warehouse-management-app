959e81da479a7d955f4e41f6034c54cdae4207d9
T.Sanyi <s.truczkai@gmail.com>
2023-05-01 15:33:07 +0200

    add more data

--- src/test/java/com/orderandwarehouse/app/data/TestComponentDto.java
+++ src/test/java/com/orderandwarehouse/app/data/TestComponentDto.java
@@ -4,8 +4,9 @@ import com.orderandwarehouse.app.model.Type;
 import com.orderandwarehouse.app.model.dto.ComponentDto;
 
 public interface TestComponentDto {
-    ComponentDto component1 = ComponentDto.builder()
-            .name("component1")
-            .type(Type.SMD)
-            .build();
+    ComponentDto invalidComponentDto1 = ComponentDto.builder().name("").type(Type.SMD).build();
+    ComponentDto invalidComponentDto2 = ComponentDto.builder().name("1".repeat(121)).type(Type.SMD).build();
+    ComponentDto componentDto1 = ComponentDto.builder().name("component1").type(Type.SMD).build();
+    ComponentDto componentDto2 = ComponentDto.builder().name("component2").type(Type.SMD).build();
+    ComponentDto componentDto3 = ComponentDto.builder().name("component3").type(Type.THD).build();
 }
