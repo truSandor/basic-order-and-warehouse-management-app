6f4d9d992fd21e44a5c70f45299bf35d532bf65a
T.Sanyi <s.truczkai@gmail.com>
2023-05-01 20:25:06 +0200

    add more tests

--- src/test/java/com/orderandwarehouse/app/integration/StorageUnitIntegrationTests.java
+++ src/test/java/com/orderandwarehouse/app/integration/StorageUnitIntegrationTests.java
@@ -2,6 +2,7 @@ package com.orderandwarehouse.app.integration;
 
 
 import com.orderandwarehouse.app.model.Component;
+import com.orderandwarehouse.app.model.StorageUnit;
 import com.orderandwarehouse.app.model.Type;
 import com.orderandwarehouse.app.model.dto.ComponentDto;
 import com.orderandwarehouse.app.model.dto.StorageUnitDto;
@@ -11,12 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -62,4 +69,49 @@ public class StorageUnitIntegrationTests {
         assertEquals(Collections.emptyList(), List.of(restTemplate.getForObject(entityUrl, Component[].class)));
     }
 
+    @Test
+    void emptyDatabase_addValidStorageUnit_returnsAddedStorageUnit() {
+        HttpEntity<StorageUnitDto> request = new HttpEntity<>(storageUnitDto1);
+        StorageUnit result = restTemplate.postForObject(entityUrl, request, StorageUnit.class);
+        assertNotNull(result.getId());
+        assertEquals(1L, result.getId());
+        assertEquals(storageUnitDto1.getRow(), result.getRow());
+        assertEquals(storageUnitDto1.getColumn(), result.getColumn());
+        assertEquals(storageUnitDto1.getShelf(), result.getShelf());
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), result.getDateAdded().truncatedTo(ChronoUnit.SECONDS));
+        assertEquals(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), result.getDateModified().truncatedTo(ChronoUnit.SECONDS));
+    }
+
+    @Test
+    void emptyDatabase_addInvalidStorageUnitDto1_returnsBAD_REQUEST() {
+        HttpEntity<StorageUnitDto> request = new HttpEntity<>(invalidStorageUnitDto1);
+        ResponseEntity<Object> result = restTemplate.postForEntity(entityUrl, request, Object.class);
+        Map<String, String> expectedBody = new HashMap<>() {{
+            put("row", "Needs to be greater or equal to 1!");
+        }};
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals(expectedBody, result.getBody());
+    }
+
+    @Test
+    void emptyDatabase_addInvalidStorageUnitDto2_returnsBAD_REQUEST() {
+        HttpEntity<StorageUnitDto> request = new HttpEntity<>(invalidStorageUnitDto2);
+        ResponseEntity<Object> result = restTemplate.postForEntity(entityUrl, request, Object.class);
+        Map<String, String> expectedBody = new HashMap<>() {{
+            put("componentId", "Needs to be greater or equal to 1!");
+        }};
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals(expectedBody, result.getBody());
+    }
+
+    @Test
+    void emptyDatabase_addInvalidStorageUnitDto3_returnsBAD_REQUEST() {
+        HttpEntity<StorageUnitDto> request = new HttpEntity<>(invalidStorageUnitDto3);
+        ResponseEntity<Object> result = restTemplate.postForEntity(entityUrl, request, Object.class);
+        Map<String, String> expectedBody = new HashMap<>() {{
+            put("quantity", "Needs to be greater or equal to 0.0!");
+        }};
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals(expectedBody, result.getBody());
+    }
 }
