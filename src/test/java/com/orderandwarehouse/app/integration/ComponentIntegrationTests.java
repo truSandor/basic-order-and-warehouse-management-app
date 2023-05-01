d148ed147122f8d3f13280ed92999291602985df
T.Sanyi <s.truczkai@gmail.com>
2023-05-01 15:34:30 +0200

    add more tests

--- src/test/java/com/orderandwarehouse/app/integration/ComponentIntegrationTests.java
+++ src/test/java/com/orderandwarehouse/app/integration/ComponentIntegrationTests.java
@@ -9,16 +9,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.orderandwarehouse.app.data.TestComponentDto.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -40,19 +41,52 @@ public class ComponentIntegrationTests {
     }
 
     @Test
-    void emptyDatabase_getAll_shouldReturnEmptyList() {
+    void emptyDatabase_getAll_returnEmptyList() {
         assertEquals(Collections.emptyList(), List.of(restTemplate.getForObject(entityUrl, Component[].class)));
     }
 
     @Test
-    void emptyDatabase_addOne_shouldReturnAddedApprentice() {
-        HttpEntity<ComponentDto> request = new HttpEntity<>(component1);
+    void emptyDatabase_addValidComponent_returnsAddedComponent() {
+        HttpEntity<ComponentDto> request = new HttpEntity<>(componentDto1);
         Component result = restTemplate.postForObject(entityUrl, request, Component.class);
-        assertEquals(component1.getName(), result.getName());
-        assertEquals(component1.getType(), result.getType());
+        assertNotNull(result.getId());
+        assertEquals(1L, result.getId());
+        assertEquals(componentDto1.getName(), result.getName());
+        assertEquals(componentDto1.getType(), result.getType());
         assertTrue(LocalDateTime.now().isAfter(result.getDateAdded()));
         assertTrue(LocalDateTime.now().minusSeconds(2).isBefore(result.getDateAdded()));
         assertTrue(LocalDateTime.now().isAfter(result.getDateModified()));
         assertTrue(LocalDateTime.now().minusSeconds(2).isBefore(result.getDateModified()));
+    }
+
+    @Test
+    void emptyDatabase_addInvalidComponent01_returnsError() {
+        HttpEntity<ComponentDto> request = new HttpEntity<>(invalidComponentDto1);
+        ResponseEntity<Object> result = restTemplate.postForEntity(entityUrl, request, Object.class);
+        Map<String, String> expectedBody = new HashMap<>() {{
+            put("name", "Name must not be blank!");
+        }};
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals(expectedBody, result.getBody());
+    }
+
+    @Test
+    void emptyDatabase_addInvalidComponent02_returnsError() {
+        HttpEntity<ComponentDto> request = new HttpEntity<>(invalidComponentDto2);
+        ResponseEntity<Object> result = restTemplate.postForEntity(entityUrl, request, Object.class);
+        Map<String, String> expectedBody = new HashMap<>() {{
+            put("name", "Max 120 characters!");
+        }};
+        assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+        assertEquals(expectedBody, result.getBody());
+    }
+
+    @Test
+    void someComponentStored_getAll_returnsAll() {
+        Set<ComponentDto> testData = Set.of(componentDto1, componentDto2, componentDto3);
+        testData.forEach(data -> restTemplate.postForObject(entityUrl, data, Component.class));
+        Component[] result = restTemplate.getForObject(entityUrl, Component[].class);
+        Set<String> componentDtoNames = testData.stream().map(ComponentDto::getName).collect(Collectors.toSet());
+        assertTrue(Arrays.stream(result).map(Component::getName).allMatch(componentDtoNames::contains));
     }
 }
