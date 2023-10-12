1bfe82e630b58f5a4e1ea0cc68045a8fbfd8d2b4
T.Sanyi <s.truczkai@gmail.com>
2023-10-12 20:48:23 +0200

    fix docker related files

--- docker_postgres_run.sh
+++ docker_postgres_run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name cont_psql_oaw_db -p 5433:5432 -e POSTGRES_DB=oaw_db -e POSTGRES_USER=person -e POSTGRES_PASSWORD=person --network oaw_network -d postgres:alpine3.17
+docker run --name cont_psql_oaw_db-e POSTGRES_DB=oaw_db -e POSTGRES_USER=person -e POSTGRES_PASSWORD=person --network oaw_network -d postgres:alpine3.17
